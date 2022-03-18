56b8db47a4717ee142896648dd6d95cfa1ca972b
unknown <3933714051@bitraipur.ac.in>
2022-03-18 23:52:50 +0530

    added the icons in the cart item

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
 class CartItem extends React.Component {
+    constructor () {
+        super();
+        this.state = {
+            price: 999,
+            title: 'Mobile Phone',
+            qty: 1,
+            img: ''
+        }
+    }
+
   render () {
+    const { price, title, qty } = this.state;
     return (
       <div className="cart-item">
         <div className="left-block">
@@ -13,6 +24,9 @@ class CartItem extends React.Component {
           <div style={ { color: '#777' } }>Qty: 1</div>
           <div className="cart-item-actions">
             {/* Buttons */}
+            <img alt="increase" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/992/992651.png" />
+            <img alt="decrease" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/992/992683.png" />
+            <img alt="delete" className="action-icons" src="https://cdn-icons.flaticon.com/png/512/3687/premium/3687412.png?token=exp=1647628420~hmac=3baf762fdc2d215e4448b058a2de5cde" />
           </div>
         </div>
       </div>
