2a7a295ebcffe5794542d7db8b15c8b4bdaad704
unknown <3933714051@bitraipur.ac.in>
2022-03-20 23:41:14 +0530

    added some corrections

--- src/App.js
+++ src/App.js
@@ -31,74 +31,74 @@ class App extends React.Component {
              }
         ] 
     }
-}
+  }
 
-handleIncreaseQuantity = (product)=>{
-    console.log("hey increase the quantity of the ", product)
+  handleIncreaseQuantity = (product)=>{
+      console.log("hey increase the quantity of the ", product)
+      const {products} = this.state;
+      const index = products.indexOf(product);
+
+      products[index].qty +=1;
+
+      this.setState({
+          products:products
+          //products
+          // the above line means the same as its above line becasue it has same values
+      })
+
+  }
+
+  handleDecreaseQuantity = (product)=>{
+      console.log("hey decrease the quantity of the ", product)
+      
+      const {products} = this.state;
+      const index = products.indexOf(product);
+
+      if (products[index].qty == 0){
+              return;
+          }
+      products[index].qty -=1;
+
+      this.setState({
+          products:products
+          //products
+          // the above line means the same as its above line becasue it has same values
+      })  
+
+  }
+
+  handleDeletePorduct = (id)=>{
+      console.log("hey delete the product with id ", id)
+      
+      const {products} = this.state;
+      const items = products.filter((item)=>item.id!==id)
+
+      this.setState({
+          products:items
+
+      })
+
+  }
+
+  getCartCount = ()=>{
     const {products} = this.state;
-    const index = products.indexOf(product);
 
-    products[index].qty +=1;
-
-    this.setState({
-        products:products
-        //products
-        // the above line means the same as its above line becasue it has same values
+    let count = 0;
+    products.forEach((product)=>{
+      count += product.qty;
     })
+    return count;
+  }
 
-}
-
-handleDecreaseQuantity = (product)=>{
-    console.log("hey decrease the quantity of the ", product)
-    
+  getCartTotal = ()=>{
     const {products} = this.state;
-    const index = products.indexOf(product);
 
-    if (products[index].qty == 0){
-            return;
-        }
-    products[index].qty -=1;
-
-    this.setState({
-        products:products
-        //products
-        // the above line means the same as its above line becasue it has same values
+    let cartTotal = 0;
+    products.map((product)=>{
+      cartTotal = cartTotal + product.qty * product.price
     })
-
-}
-
-handleDeletePorduct = (id)=>{
-    console.log("hey delete the product with id ", id)
-    
-    const {products} = this.state;
-    const items = products.filter((item)=>item.id!==id)
-
-    this.setState({
-        products:items
-
-    })
-
-}
-
-getCartCount = ()=>{
-  const {products} = this.state;
-
-  let count = 0;
-  products.forEach((product)=>{
-    count += product.qty;
-  })
-  return count;
-}
-
-getCartTotal = ()=>{
-  const {products} = this.state;
-
-  let cartTotal = 0;
-  products.map((product)=>{
-    cartTotal = cartTotal + product.qty * product.price
-  })
-  return cartTotal;
-}
+    return cartTotal;
+  }
 
 
   render(){
@@ -118,6 +118,7 @@ getCartTotal = ()=>{
       </div>
     );
   }
+  
 }
 
 export default App;
