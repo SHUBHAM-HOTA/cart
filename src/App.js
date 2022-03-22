2e5d93d20afb7473997eeb9aabf1e9f6ceb7cc16
unknown <3933714051@bitraipur.ac.in>
2022-03-22 19:48:11 +0530

    added the increse qty funciton with firebase and added the add item button now comment

--- src/App.js
+++ src/App.js
@@ -12,21 +12,34 @@ class App extends React.Component {
         products : [],
         loading: true
     }
+    this.db = firebase.firestore()
   }
 
   componentDidMount() {
-    firebase
-      .firestore()
+    // firebase
+    //   .firestore()
+    //   .collection("products")
+    //   .get()
+    //   .then(snapshot => {
+    //     const products = snapshot.docs.map(doc => {
+    //       const data = doc.data();
+    //       data["id"] = doc.id;
+    //       return data;
+    //     });
+    //     this.setState({ products: products, loading: false });
+    //   });
+
+    this.db
       .collection("products")
-      .get()
-      .then(snapshot => {
+      // onsnapshot is called whenever something is changed in the producnt collection in database
+      .onSnapshot((snapshot) => {
         const products = snapshot.docs.map(doc => {
           const data = doc.data();
           data["id"] = doc.id;
           return data;
         });
         this.setState({ products: products, loading: false });
-      });
+      })
   }
 
   handleIncreaseQuantity = (product)=>{
@@ -34,14 +47,26 @@ class App extends React.Component {
       const {products} = this.state;
       const index = products.indexOf(product);
 
-      products[index].qty +=1;
+      //now we dont need to increse the quantity in states we will update in database and onSnapshot will change the Dom
+      // products[index].qty +=1;
 
-      this.setState({
-          products:products
-          //products
-          // the above line means the same as its above line becasue it has same values
+      // this.setState({
+      //     products:products
+      //     //products
+      //     // the above line means the same as its above line becasue it has same values
+      // })
+
+      const docRef = this.db.collection("products").doc(products[index].id);
+      docRef
+      .update({
+        qty:products[index].qty + 1
       })
-
+      .then(()=>{
+        console.log("document updated successfully")
+      })
+      .catch((error)=>{
+        console.log("error:",error)
+      })
   }
 
   handleDecreaseQuantity = (product)=>{
@@ -99,12 +124,29 @@ class App extends React.Component {
     return cartTotal;
   }
 
+  addProduct = ()=>{
+    this.db
+    .collection('products')
+    .add({
+      img:"",
+      price:900,
+      qty:3,
+      title:"Washing machine"
+    })
+    .then((docRef)=>{
+      console.log("Product has been added",docRef)
+    })
+    .catch((error)=>{
+      console.log("Error",error)
+    })
+  }
 
   render(){
     const {products, loading} = this.state;
     return (
       <div className="App">
         <Navbar count = {this.getCartCount()} />
+        {/* <button onClick={this.addProduct}>Add a product</button> */}
         <Cart 
           products = {products}
           onIncreaseQuantity = {this.handleIncreaseQuantity}
